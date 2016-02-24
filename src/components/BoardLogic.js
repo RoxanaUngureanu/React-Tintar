08b0e0f66b88b3a041a1b7f88130553b9feca95b
Roxana Ungureanu <ungureanu.roxi@yahoo.com>
2016-02-24 18:03:57 +0200

    set points based on current player

--- src/components/BoardLogic.js
+++ src/components/BoardLogic.js
@@ -9,18 +9,41 @@ var BoardLogic = React.createClass ({
   getInitialState: function(){
     var board=[];
     var size = 400;
+    for (var i=0; i<24;i++){
+      board.push({initPlayer:-1})
+    }
     return {
       board:board,
       white:9,
       black:9,
-      player:1,
+      currentPlayer:1,
       size:size
     }
   },
+
+  onPointClick:function(index){
+    var initPlayer = this.state.board[index].initPlayer;
+    var currentPlayer = this.state.currentPlayer;
+    if (initPlayer === -1) {
+      if (currentPlayer === 1) {
+        this.state.currentPlayer = 0;
+        this.state.board[index].initPlayer = 1;
+        this.state.white -= 1;
+      } else if (currentPlayer === 0) {
+        this.state.currentPlayer = 1;
+        this.state.board[index].initPlayer = 0;
+        this.state.black -= 1;
+      }
+      this.forceUpdate();
+    }
+  },
+
   render: function(){
     var points=[];
     var top = 0;
     var left = 0;
+    var background = '#0d290a';
+    var currentPlayer = this.state.currentPlayer;
     for (var i=0; i<24; i++){
       if (i<8){
         top = this.topPos((i-this.startPos(i)),400);
@@ -32,7 +55,16 @@ var BoardLogic = React.createClass ({
         top = this.topPos((i-this.startPos(i)),200)+100;
         left = this.leftPos((i-this.startPos(i)),200)+100;
       }
-        points.push(<Point key={i} index={i} top={top} left={left} onClick={this.onPointClick}/>);
+
+      if (this.state.board[i].initPlayer === 1) {
+        background="yellow"
+      } else if (this.state.board[i].initPlayer === 0) {
+        background="magenta"
+      } else {
+        background = '#0d290a';
+      }
+
+        points.push(<Point key={i} index={i} top={top} left={left} currentPlayer={currentPlayer} background={background} onClick={this.onPointClick}/>);
     }
     return (
       <div>
@@ -71,11 +103,6 @@ var BoardLogic = React.createClass ({
     } else {
       return size
     }
-  },
-
-  onPointClick:function(){
-
-      console.log("clickity-click")
   }
 });
 
--- src/components/Points.js
+++ src/components/Points.js
@@ -5,12 +5,13 @@ import React from 'react';
 
 var Point = React.createClass ({
   onClick:function(){
-    this.props.onClick(this.props.index,this.props.top, this.props.left)
+    this.props.onClick(this.props.index,this.props.top, this.props.background, this.props.left)
   },
   render: function(){
     var style = {
       left: this.props.left,
-      top: this.props.top
+      top: this.props.top,
+      background:this.props.background
     };
     return (
         <div className='points' style={style} onClick={this.onClick}></div>
