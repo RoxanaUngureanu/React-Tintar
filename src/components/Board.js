cba1168b8e364ecd18a1f05c99bc6c5ee3c23cbe
Roxana Ungureanu <ungureanu.roxi@yahoo.com>
2016-02-26 12:08:00 +0200

    refactor with es6

--- src/components/Board.js
+++ src/components/Board.js
@@ -6,26 +6,26 @@ import BoardDesign from './BoardDesign';
 import Point from './Points';
 
 class Board extends React.Component {
-  constructor(props){
-    super(props);
-    var board=[];
+  constructor(){
+    super();
+    const board=[];
     let size = 400;
     for (let i=0; i<24;i++){
       board.push({initPlayer:-1})
     }
     this.state = {
-      board:board,
+      board,
       white:9,
       green:9,
       currentPlayer:1,
-      size:size,
+      size,
       status:'Start your turn'
     }
   }
 
   onPointClick(index){
-    var initPlayer = this.state.board[index].initPlayer;
-    var currentPlayer = this.state.currentPlayer;
+    const initPlayer = this.state.board[index].initPlayer;
+    const currentPlayer = this.state.currentPlayer;
     if (initPlayer === -1) {
       if ((currentPlayer === 1) && (this.state.white > 0)) {
         this.state.currentPlayer = 0;
@@ -43,13 +43,13 @@ class Board extends React.Component {
   }
 
   render(){
-    var points=[];
-    var top = 0;
-    var left = 0;
-    var bgImage = "url('http://coryg89.github.io/assets/themes/img/wood-texture.jpg')";
-    var background = bgImage;
-    var currentPlayer = this.state.currentPlayer;
-    for (var i=0; i<24; i++){
+    const points=[];
+    let top = 0;
+    let left = 0;
+    const bgImage = "url('http://coryg89.github.io/assets/themes/img/wood-texture.jpg')";
+    let background = bgImage;
+    const currentPlayer = this.state.currentPlayer;
+    for (let i=0; i<24; i++){
       if (i<8){
         top = this.topPos((i-this.startPos(i)),400);
         left = this.leftPos((i-this.startPos(i)),400);
@@ -69,13 +69,17 @@ class Board extends React.Component {
         background = bgImage;
       }
 
-        points.push(<Point key={i} index={i} top={top} left={left} currentPlayer={currentPlayer} background={background} onClick={this.onPointClick}/>);
+        points.push(<Point key={i} index={i} top={top} left={left}
+                           currentPlayer={currentPlayer}
+                           background={background}
+                           onClick={this.onPointClick.bind(this)}/>);
     }
+
     return (
       <div>
         <h1>Nine Men's Morris</h1>
         <p className="status">{this.state.status}</p>
-        <button className="button"  onClick={this.resetGame}>Reset the game!</button>
+        <button className="button"  onClick={this.resetGame.bind(this)}>Reset the game!</button>
         <div className="parent-div">
           {points}
             <BoardDesign/>
@@ -85,10 +89,17 @@ class Board extends React.Component {
           <p>Green: {this.state.green}</p>
         </div>
       </div>
-    );
+    )
   }
+
   resetGame(){
-    this.setState(this.getInitialState())
+    this.state.white = 9;
+    this.state.green = 9;
+    this.state.currentPlayer = 1;
+    for (let i=0; i<24;i++){
+      this.state.board[i].initPlayer = -1;
+    }
+    this.forceUpdate();
   }
 
   startPos(index){
